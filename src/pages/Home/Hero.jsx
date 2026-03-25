ff900548802ac235fdbb075f92631403da672886
Ruslan Chyhryn <rusrus.0110@gmail.com>
2026-03-25 21:35:47 +0100

    add Hero section

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,85 +1,150 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
+
+import logo from "../../assets/hero/logo.png";
+import oneText from "../../assets/hero/ONE.png";
+import airText from "../../assets/hero/AIR.png";
+import upToText from "../../assets/hero/up-to.png";
+import offText from "../../assets/hero/50-off.png";
+import shoesTop from "../../assets/hero/shoes-2.png";
+import shoesBottom from "../../assets/hero/shoes.png";
+
+const TOP_SECTION_HEIGHT = 195;
+const BOTTOM_SECTION_HEIGHT = 180;
 
 const Hero = () => {
   return (
-    <Box
-      sx={{
-        mt: 3,
-        height: { xs: 180, md: 260 },
-        borderRadius: 3,
-        overflow: "hidden",
-        position: "relative",
-        background: "#5c78a5",
-      }}
-    >
-      <Box
-        component="img"
-        src="/assets/hero/AIR.png"
-        sx={{
-          position: "absolute",
-          left: 20,
-          top: "50%",
-          transform: "translateY(-50%)",
-          opacity: 0.15,
-          height: 90,
-        }}
-      />
+    <Box sx={{ pt: 4 }}>
+      <Container>
+        <Box sx={{ overflow: "hidden", position: "relative" }}>
 
-      <Box
-        component="img"
-        src="/assets/hero/ONE.png"
-        sx={{
-          position: "absolute",
-          left: 30,
-          bottom: 20,
-          height: 60,
-        }}
-      />
+          {/* ================= TOP SECTION ================= */}
+          <Box
+            sx={{
+              position: "relative",
+              height: TOP_SECTION_HEIGHT,
+              background: "linear-gradient(180deg, #6f8fc5 0%, #4f6fa5 100%)",
+              zIndex: 2,
+            }}
+          >
+            {/* LOGO  */}
+            <Box
+              sx={{
+                position: "absolute",
+                top: 10,
+                left: 15,
+                width: 42,
+                height: 42,
+                backgroundColor: "#000",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                zIndex: 5,
+              }}
+            >
+              <Box
+                component="img"
+                src={logo}
+                sx={{
+                  width: 24,
+                }}
+              />
+            </Box>
 
-      <Box
-        component="img"
-        src="/assets/hero/UP_TO.png"
-        sx={{
-          position: "absolute",
-          right: 120,
-          top: 40,
-          height: 20,
-        }}
-      />
+            {/* ONE */}
+            <Box
+              component="img"
+              src={oneText}
+              sx={{
+                position: "absolute",
+                left: 20,
+                bottom: 10,
+                width: 260,
+                opacity: 0.2,
+                zIndex: 1,
+              }}
+            />
 
-      <Box
-        component="img"
-        src="/assets/hero/50_OFF.png"
-        sx={{
-          position: "absolute",
-          right: 40,
-          top: 60,
-          height: 60,
-        }}
-      />
+            {/* SHOES TOP */}
+            <Box
+              component="img"
+              src={shoesTop}
+              sx={{
+                position: "absolute",
+                left: 200,
+                top: 1,
+                width: 260,
+                zIndex: 3,
+              }}
+            />
 
-      <Box
-        component="img"
-        src="/assets/hero/shoes.png"
-        sx={{
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%, -50%) rotate(-10deg)",
-          height: { xs: 120, md: 180 },
-        }}
-      />
+            {/* UP TO */}
+            <Box
+              component="img"
+              src={upToText}
+              sx={{
+                position: "absolute",
+                right: 160,
+                top: 20,
+                width: 70,
+                zIndex: 3,
+              }}
+            />
 
-      <Box
-        component="img"
-        src="/assets/hero/logo.png"
-        sx={{
-          position: "absolute",
-          top: 10,
-          left: 10,
-          height: 30,
-        }}
-      />
+            {/* 50% OFF */}
+            <Box
+              component="img"
+              src={offText}
+              sx={{
+                position: "absolute",
+                right: 80,
+                top: 50,
+                width: 240,
+                zIndex: 3,
+              }}
+            />
+          </Box>
+
+          {/* ================= BOTTOM SECTION ================= */}
+          <Box
+            sx={{
+              position: "relative",
+              height: BOTTOM_SECTION_HEIGHT,
+              background: "linear-gradient(180deg, #dcdcdc 0%, #bfbfbf 100%)",
+              mt: "-40px", 
+              zIndex: 1,
+            }}
+          >
+            {/* AIR */}
+            <Box
+              component="img"
+              src={airText}
+              sx={{
+                position: "absolute",
+                left: 20,
+                bottom: 10,
+                width: 260,
+                opacity: 0.2,
+              }}
+            />
+
+            {/* SHOES BOTTOM */}
+            <Box
+              component="img"
+              src={shoesBottom}
+              sx={{
+                position: "absolute",
+                left: "70%",
+                transform: "translateX(-50%)",
+                width: "70%",
+                maxWidth: 400,
+                bottom: "-10px",
+                zIndex: 2,
+              }}
+            />
+          </Box>
+
+        </Box>
+      </Container>
     </Box>
   );
 };
